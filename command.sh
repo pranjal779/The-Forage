2bcd88b7cc268cf06f0cac3acd4c0494d50f8659
Pranjal Shukla <pranjal779@gmail.com>
2023-02-03 19:42:05 -0500

    Task 1 done

--- command.sh
+++ command.sh
@@ -1,31 +1,44 @@
-step 1:
+# wsl nano
+# step 1:
 npx create-react-app my-app
 npm start
 npm test
 Ctrl + c (to stop)
 npm start
 
-step 2:
+# wsl nano command.sh
+# step 2:
 git clone https://github.com/pranjal779/The-Forage.git
 git add -A
 git status
 git commit -m
 git push origin main
 
-step 3:
+# wsl nano command.sh
+# step 3:
 git branch
 git branch update_logo
 git checkout update_logo
 
-step4:
-replaced the logo at the index.html
-
-step 5:
-replaced the existing logo in App.js
-
-
-step 6:
+#wsl nano command.sh
+# step4:
+# replaced the logo at the index.html
+#
+# step 5:
+# replaced the existing logo in App.js
+#
+#
+# step 6:
 git add -A
 git commit -m ""
 
+# wsl nan0 command.sh
+# step 7 & 8:
+git add -A
+git commit -m ""
+git checkout main
+git push origin main update_logo
+# this created a PR and accepted in my own repo as no repo link was given.
 
+# step 9:
+# done
