3fa12b100a5f2b3d817d7449386d49875f9f8bdd
Pranjal Shukla <pranjal779@gmail.com>
2023-02-03 19:17:03 -0500

    did all the steps until step 6

---
+++ command.sh
@@ -0,0 +1,26 @@
+step 1:
+npx create-react-app my-app
+npm start
+npm test
+Ctrl + c (to stop)
+npm start
+
+step 2:
+git clone https://github.com/pranjal779/The-Forage.git
+git add -A
+git status
+git commit -m
+git push origin main
+
+step 3:
+git branch
+git branch update_logo
+git checkout update_logo
+
+step4:
+replaced the logo at the index.html
+
+step 5:
+replaced the existing logo in App.js
+
+
